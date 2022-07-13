2879efff9ae19af29367cd09ea6f68217e0f06bb
h224seno <108775323+h224seno@users.noreply.github.com>
2022-07-13 11:15:09 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php 
+$b='abc';
+  foreach(file('/usr/share/dict/linux.words') as $line){
+    $pos=strpos($line,$b);
+    if ($pos !== false){
+     echo $line;
+    }
+    //関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+  } 
+?>
